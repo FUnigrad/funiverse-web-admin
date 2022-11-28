56dabde9d9c3e15cadea71007a16a463688342d8
Nguyen Tran <nguyen_tran@datahouse.com>
2022-11-28 20:37:36 +0700

    feat: init

---
+++ src/layout/index.tsx
@@ -0,0 +1,16 @@
+import Footer from './Footer';
+import Header from './Header';
+interface PublicLayoutProps {
+  children: React.ReactNode;
+}
+function PublicLayout({ children }: PublicLayoutProps) {
+  return (
+    <div className="layout">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default PublicLayout;
